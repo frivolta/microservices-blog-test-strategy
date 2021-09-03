a2f61bc5d2a764519ecfc820e2efd7825b265637
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-09-03 16:43:54 +0200

    Adding k8s depl / serv / node port

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,10 +10,10 @@ const events = [];
 app.post("/events", function (req, res) {
   const event = req.body;
   events.push(event);
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  /* axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
   });
   axios.post("http://localhost:4002/events", event).catch((err) => {
@@ -21,7 +21,7 @@ app.post("/events", function (req, res) {
   });
   axios.post("http://localhost:4003/events", event).catch((err) => {
     console.log(err.message);
-  });
+  }); */
 
   res.send({ status: "OK" });
 });
--- posts/index.js
+++ posts/index.js
@@ -20,7 +20,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -37,5 +37,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("1");
   console.log("listening on 4000");
 });
